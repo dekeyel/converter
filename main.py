a2d73d2c5fc6bae466a9fb871f76cc5af3d8e7bb
dekeyel <dekeyel>
2020-03-09 18:36:50 +0300

    add to portfolio

---
+++ main.py
@@ -0,0 +1,268 @@
+from views import *
+
+
+class Menu:
+    dir = None
+
+    def __init__(self):
+        self._args = ['Выход']
+
+    def case(self, arg):
+        if arg == str(len(self._args)):
+            return 1
+
+    def __len__(self):
+        return len(self._args)
+
+    def __str__(self):
+        s = '1) {}'.format(self._args[0])
+        for i in range(1, len(self._args)):
+            s = '{}\n{}) {}'.format(s, i + 1, self._args[i])
+
+        return '{}\n{}'.format(s, 'Выберете пункт: ')
+
+
+class RepeatDirMenu(Menu):
+    def __init__(self):
+        Menu.__init__(self)
+        self._args = [
+            'Указать путь ещё раз.',
+            self._args[-1]
+        ]
+
+    def case(self, arg):
+        if arg == '1':
+            return arg
+        return Menu.case(self, arg)
+
+
+class PrintFile(Menu):
+    def __init__(self):
+        Menu.__init__(self)
+        if Menu.dir:
+            mass = Menu.dir.get_files()
+        else:
+            mass = []
+
+        self._args = mass + [
+            'Указать путь до нового файла.',
+            self._args[-1]
+        ]
+
+    def case(self, arg):
+        if arg == str(len(self._args) - 1):
+            path = input('Укажите путь до файла: ')
+
+            if len(path.split('/')) == 1:
+                tmp = File(path)
+            else:
+                tmp = File(path.split('/')[-1], path[:(-1 * len(path.split('/')[-1])) - 1])
+            if tmp:
+                print(tmp.__str__().decode(tmp.encoding))
+            else:
+                print('Неудача')
+                Now_menu = RepeatDirMenu()
+                while True:
+                    print(Now_menu)
+                    switch = Now_menu.case(input())
+                    if switch == '1':
+                        self.case(arg)
+                        break
+                    elif switch:
+                        break
+
+        elif not Menu.case(self, arg):
+            try:
+                print(Menu.dir.show(int(arg) - 1).__str__().decode(Menu.dir.show(int(arg) - 1).encoding))
+            except:
+                print('Неверно выбран пункт.')
+
+        return Menu.case(self, arg)
+
+
+class PrintCodeFile(PrintFile):
+    def __init__(self):
+        PrintFile.__init__(self)
+
+    # Переопределяем функцию case из класса Menu
+    def case(self, arg):
+        if arg == str(len(self._args) - 1):
+            path = input('Укажите путь до файла: ')
+
+            if len(path.split('/')) == 1:
+                tmp = File(path)
+            else:
+                tmp = File(path.split('/')[-1], path[:(-1 * len(path.split('/')[-1])) - 1])
+            if tmp:
+                print('Кодировка файла {} {}'.format(tmp.file_name, tmp.encoding))
+            else:
+                print('Неудача')
+                Now_menu = RepeatDirMenu()
+                while True:
+                    print(Now_menu)
+                    switch = Now_menu.case(input())
+                    if switch == '1':
+                        self.case(arg)
+                        break
+                    elif switch:
+                        break
+        elif not Menu.case(self, arg):
+            try:
+                print('Кодировка файла {} {}'.format(Menu.dir.show(int(arg) - 1).file_name,
+                                                     Menu.dir.show(int(arg) - 1).encoding))
+            except:
+                print('Неверно выбран пункт.')
+
+        return Menu.case(self, arg)
+
+
+class PrintEncodings(Menu):
+    def __init__(self):
+        Menu.__init__(self)
+        self._args = Convert.encodings + [
+            'Отмена'
+        ]
+
+    def case(self, arg):
+        try:
+            if int(arg) != len(self._args):
+                return self._args[int(arg) - 1]
+            else:
+                Menu.case(self, arg)
+        except:
+            print('Неверно выбран пункт.')
+
+
+class ChangeEncoding(PrintFile):
+    def __init__(self):
+        PrintFile.__init__(self)
+
+    def case(self, arg):
+        if arg == str(len(self._args) - 1):
+            path = input('Укажите путь до файла: ')
+
+            if len(path.split('/')) == 1:
+                tmp = File(path)
+            else:
+                tmp = File(path.split('/')[-1], path[:(-1 * len(path.split('/')[-1])) - 1])
+            if tmp:
+                Now_menu = PrintEncodings()
+                while True:
+                    print('Для файла {} с кодировкой {} выберете новую кодировку из списка'.format(tmp.file_name,
+                                                                                                   tmp.encoding))
+                    print(Now_menu)
+                    switch = Now_menu.case(input())
+                    if switch:
+                        if switch != 1:
+                            tmp.write(Convert.convert(tmp.__str__(), switch))
+                            break
+                        else:
+                            break
+
+            else:
+                print('Неудача')
+                Now_menu = RepeatDirMenu()
+                while True:
+                    print(Now_menu)
+                    switch = Now_menu.case(input())
+                    if switch == '1':
+                        self.case(arg)
+                        break
+                    elif switch:
+                        break
+        elif not Menu.case(self, arg):
+            try:
+                Now_menu = PrintEncodings()
+                while True:
+                    print('Для файла {} с кодировкой {} выберете новую кодировку из списка'.format(
+                        Menu.dir.show(int(arg) - 1).file_name,
+                        Menu.dir.show(int(arg) - 1).encoding)
+                    )
+                    print(Now_menu)
+                    switch = Now_menu.case(input())
+                    if switch:
+                        if switch != 1:
+                            Menu.dir.show(int(arg) - 1).write(Convert.convert(Menu.dir.show(int(arg) - 1).__str__(),
+                                                                              switch))
+                            break
+                        else:
+                            break
+            except:
+                print('Неверно выбран пункт.')
+
+        return Menu.case(self, arg)
+
+
+class MainMenu(Menu):
+    def __init__(self):
+        Menu.__init__(self)
+        self._args = [
+            'Выбрать дирректорию.',
+            'Вывести содержание файла.',
+            'Вывести кодировку файла.',
+            'Вывести все файлы и папки в дирректории.',
+            'Вывести файлы, с возможностью смены кодировки.',
+            'Изменить кодировку файла.',
+            self._args[-1]
+        ]
+
+    def case(self, arg):
+        if arg == '1':
+            path = input('Введите дирректорию, для выбора текущего каталога нажмите enter: ')
+            try:
+                Menu.dir = Dir(path)
+                print('Успешно')
+            except:
+                print('Неудача')
+                Now_menu = RepeatDirMenu()
+                while True:
+                    print(Now_menu)
+                    switch = Now_menu.case(input())
+                    if switch == arg:
+                        self.case(arg)
+                        break
+                    elif switch:
+                        break
+        elif arg == '2':
+            Now_menu = PrintFile()
+            while True:
+                print(Now_menu)
+                if Now_menu.case(input()):
+                    break
+        elif arg == '3':
+            Now_menu = PrintCodeFile()
+            while True:
+                print(Now_menu)
+                if Now_menu.case(input()):
+                    break
+        elif arg == '4':
+            if Menu.dir:
+                print(Menu.dir.show_all_files())
+            else:
+                print('Директория пуста или не выбрана.')
+        elif arg == '5':
+            if Menu.dir:
+                print(Menu.dir)
+            else:
+                print('Директория пуста или не выбрана.')
+        elif arg == '6':
+            Now_menu = ChangeEncoding()
+            while True:
+                print(Now_menu)
+                if Now_menu.case(input()):
+                    break
+
+        return Menu.case(self, arg)
+
+    def __str__(self):
+        while True:
+            print(Menu.__str__(self))
+            if self.case(input()):
+                break
+
+        return 'Выход из программы'
+
+
+if __name__ == '__main__':
+    a = MainMenu()
+    print(a)
---
+++ views.py
@@ -0,0 +1,122 @@
+import os
+import re
+from chardet.universaldetector import UniversalDetector
+
+
+class File:
+    def __init__(self, file_name, path='.'):
+        self.file_name = file_name
+        self.path = path
+
+        detector = UniversalDetector()
+        with open('{}/{}'.format(self.path, self.file_name), 'rb') as lines:
+            for line in lines:
+                detector.feed(line)
+                if detector.done:
+                    break
+        detector.close()
+
+        self.encoding = detector.result['encoding']
+
+    def __new__(cls, file_name, path='.'):
+        try:
+            open('{}/{}'.format(path, file_name), 'rb')
+            return object.__new__(cls)
+        except:
+            return None
+
+    def write(self, text):
+        with open('{}/{}'.format(self.path, self.file_name), 'wb') as F:
+            F.write(text)
+
+    def __str__(self):
+        string = b''
+        with open('{}/{}'.format(self.path, self.file_name), 'rb') as lines:
+            for line in lines:
+                string = string + line
+
+        return string
+
+
+class Dir:
+    def __init__(self, path):
+        if path == '':
+            path = '.'
+        self.__path = path
+        self.all_files = os.listdir(path)
+        self.__files = []
+        for file in self.all_files:
+            if re.search(r'.txt\b', file):
+                tmp = File(file, path)
+                if tmp:
+                    self.__files.append(tmp)
+
+    def __new__(cls, path):
+        try:
+            return object.__new__(cls)
+        except:
+            return None
+
+    def show(self, ind):
+        try:
+            return self.__files[ind]
+        except:
+            return None
+
+    def get_files(self):
+        mass = []
+        for i in self.__files:
+            mass.append(i.file_name)
+        return mass
+
+    def show_all_files(self):
+        if self.__path == '.':
+            path = 'текущей дирректории'
+        else:
+            path = 'дирректории {}'.format(self.__path)
+        s = 'Объекты в {}:'.format(path)
+        if self.all_files:
+            for i in self.all_files:
+                s = '{} {}'.format(s, i)
+        else:
+            s = '{} {}'.format(s, 'Пусто')
+        return s
+
+    def __str__(self):
+        if self.__path == '.':
+            path = 'текущей дирректории'
+        else:
+            path = 'дирректории {}'.format(self.__path)
+        s = 'Файлы с возможностью изменения кодировки в {}:'.format(path)
+        if self.__files:
+            for i in self.__files:
+                s = '{} {}'.format(s, i.file_name)
+        else:
+            s = '{} {}'.format(s, 'Пусто')
+        return s
+
+
+class Convert:
+    encodings = [
+        'utf-8',
+        'utf-16',
+        'utf-32',
+        'windows-1251'
+    ]
+
+    @staticmethod
+    def show_encode(text):
+        detector = UniversalDetector()
+        detector.feed(text)
+        detector.close()
+
+        return detector.result['encoding']
+
+    @staticmethod
+    def convert(text, encoding):
+        try:
+            text = text.decode(Convert.show_encode(text))
+            text = text.encode(encoding)
+            return text
+        except:
+            print('Неудача')
